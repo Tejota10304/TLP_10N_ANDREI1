13936c34a60a84c75b2533add318b5c804683e07
Andrei_Sousa <andreisousa193@gmail.com>
2024-10-30 12:18:56 +0000

    Add files via upload
    Código que repete os números que o utilizador escolher em decrescente.

---
+++ Cronometro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int N, i;
+
+	cout << "Me de um numero: ";
+	cin >> N;
+	for (i = N; i > 0; i--)
+	{
+		cout << "\n" << i << " Segundos";
+	}
+}
