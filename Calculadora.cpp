47f6ee2ce8e6857f62f1209b8c2688b25cb64ddf
Andrei_Sousa <andreisousa193@gmail.com>
2024-10-30 13:03:37 +0000

    Add files via upload
    Esse e um código de uma calculadora, onde o utilizador coloca dois numero e a operação que deseja fazer e o programa te dar o código( + , - , * , / ).

---
+++ Calculadora.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	char op;
+	int numb1, numb2;
+	int Resultado;
+
+	cout << "Qual o primeiro numero\n";
+	cin >> numb1;
+	cout << "Qual operacao deseja fazer [ + , - , / , * ]\n";
+	cin >> op;
+	cout << "Qual o segundo numero\n";
+	cin >> numb2;
+
+	switch (op)
+	{
+	case '+':
+		Resultado= numb1  + numb2;
+		cout << "O seu resultado e " << Resultado;
+		
+	case '-':
+		Resultado = numb1 - numb2;
+		cout << "O seu resultado e " << Resultado;
+
+	case '/':
+		Resultado = numb1 / numb2;
+		cout << "O seu resultado e " << Resultado;
+
+	case '*':
+		Resultado = numb1 * numb2;
+		cout << "O seu resultado e " << Resultado;
+	default:
+		break;
+	}
+	}
+
