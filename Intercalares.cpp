268b98ee3c7d0c51d977ace6b5c2209815793357
Andrei_Sousa <andreisousa193@gmail.com>
2024-11-22 15:21:56 +0000

    Add files via upload

---
+++ Intercalares.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int batata[10];
+	int batataI[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Me de " << i + 1 << " Numero\n";
+		cin >> batataI[i];
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Me de " << i + 1 << " Numero\n";
+		cin >> batata[i];
+	}
+	for (int i = 0; i <10; i++)
+	{
+		cout << batataI[i];
+		cout << batata[i];
+	}
+}
