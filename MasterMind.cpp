cd4967e5c28a9ab3649cc9ae88fa535d779f1502
Andrei_Sousa <andreisousa193@gmail.com>
2024-11-28 14:41:57 +0000

    Update MasterMind.cpp

--- MasterMind.cpp
+++ MasterMind.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 void main()
 {
-	
-	int secretos[4], escolha[4], acertos = 0;
+
+	int secretos[4], escolha[4], acertos = 0, vezes = 0;
 	srand(time(0));
-	
-		for (int i = 0; i < 4; i++)
-		{
-			secretos[i] = rand() % 2;
-			i + 1;
-		}
+
+	for (int i = 0; i < 4; i++)
+	{
+		secretos[i] = rand() % 2;
+		i + 1;
+	}
 	do {
+		acertos = 0;
 		cout << "Adivinha os meus 4 Numero:\nApenas 0/1 \n";
 		for (int i = 0; i < 4; i++)
 		{
@@ -28,11 +29,12 @@ void main()
 				acertos++;
 			}
 		}
-		cout << "Voce acertou " << acertos<<"\n";
+		vezes++;
+		cout << "Voce acertou " << acertos << "\n";
 		if (acertos == 4)
 		{
-			cout << "Parabens voce  ganhou 1 MILHAO DE DOLARES";
+			cout << "Parabens voce  ganhou 1 MILHAO DE DOLARES\n" << "Em " << vezes << " tentativas";
 		}
 	} while (acertos < 4);
-	
+
 }
